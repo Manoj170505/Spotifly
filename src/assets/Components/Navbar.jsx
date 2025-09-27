20a04bf51eceb7447e3ababdf8ca1d706bdd41d0
Manoj M <manojfa4451e@gmail.com>
2025-09-27 11:14:16 +0530

    First Commit

--- src/assets/Components/Navbar.jsx
+++ src/assets/Components/Navbar.jsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
       <div className='flex flex-row gap-4 items-center'>
         <FaSpotify className='text-3xl' />
         <button className='bg-[#1e1e1e] w-12 h-12 flex justify-center items-center rounded-[50%] hover:bg-[#2a2a2a]'><RiHome5Fill className='text-3xl' /></button>
-        <div className='bg-[#1e1e1e] h-12 flex flex-row gap-2 items-center p-3 rounded-4xl hover:bg-[#2a2a2a] hidden md:block'><GoSearch className='text-[#808080] text-2xl'/><input className='w-100 border-none focus:outline-none' type="text" placeholder='What do you want to play?'/></div>
+        <div className='bg-[#1e1e1e] h-12 flex flex-row gap-2 items-center p-3 rounded-4xl hover:bg-[#2a2a2a]'><GoSearch className='text-[#808080] text-2xl'/><input className='w-100 border-none focus:outline-none' type="text" placeholder='What do you want to play?'/></div>
       </div>
       <div className='p-3 rounded-4xl flex flex-row gap-4 items-center text-[#b3b3b3]'>
         <a className='flex flex-row gap-3 justify-center items-center text-m hover:text-[#e4e4e4] hover:scale-105 hidden md:block' href="https://open.spotify.com/download" target='__blank'><MdOutlineDownloading />Install App</a>
